7d6781bde1d6b43e7d86b4d52087222c2b09cb81
Sauloleocadio <saulomanshu@gmail.com>
2024-10-03 09:51:44 -0300

    feature: add google search console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,10 @@ export default function Home() {
           name="description"
           content="Serviços de Estética Automotiva de Qualidade"
         />
+        <meta
+          name="google-site-verification"
+          content="3bKQtRLfIQrcaB_pF1KWc1L_E-9BFiyHfxTbY8Mh3-A"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
